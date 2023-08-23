e6a54c6e3689b0ce4cb552921efd02e0255e2de1
Hussein Mahfouz <45176416+Hussein-Mahfouz@users.noreply.github.com>
2023-08-23 16:58:56 +0100

    function to copy files

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,30 @@
 
 
 
+# --- function to copy all files with a specific pattern from one directory to another
+copy_files = function(source, dest, pattern){
+  # INPUT:
+    # source: directory where the files are
+    # dest: directory where we want to copy the files
+    # pattern: the extension of the files (e.g. ".zip", ".tif")
+  # -----------
+  # get name of files with specific pattern in a directory
+  files = list.files(source, pattern = pattern)
+  # check that we have at least one file with the specified pattern (extension)
+  if(length(files) > 0){
+    # get file path for each of the files
+    file_source = paste0(source, files)
+    # define path where we want to move the file
+    file_dest = paste0(dest, files)
+    # copy the file(s)
+    file.copy(file_source, file_dest)
+
+  } else{
+    paste0("there are no files with the pattern '", pattern, "' in ", source)
+  }
+}
+
+
 # Github releases
 
 # - upload files
