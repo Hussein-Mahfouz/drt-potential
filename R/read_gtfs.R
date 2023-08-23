360330ad0b8e3df83766ac40727199542271fb8d
Hussein Mahfouz <45176416+Hussein-Mahfouz@users.noreply.github.com>
2023-08-23 12:39:33 +0100

    add package argument for read_gtfs_feeds() function

--- R/read_gtfs.R
+++ R/read_gtfs.R
@@ -1,9 +1,10 @@
 # Function to read in multiple gtfs feeds
 library(tidyverse)
 
-read_gtfs_feeds <- function(feed_dir){
+read_gtfs_feeds <- function(feed_dir, package = "gtfstools"){
   #INPUT:
   # feed_dir: folder where gtfs feeds are stored
+  # package: which package to use (tidytransit, gtfstools)
   # OUTPUT:
   # list of gtfs feeds
   # ----- 1. get relative paths of all gtfs feeds
@@ -11,12 +12,17 @@ read_gtfs_feeds <- function(feed_dir){
   # ----- 2. read in the feeds
   # ----- check that there are zip files in the path provided
   if(length(feeds) > 0){
-    gtfs_feeds <- purrr::map(feeds, gtfstools::read_gtfs)
+    if(package == "gtfstools"){
+      gtfs_feeds <- purrr::map(feeds, gtfstools::read_gtfs)
+      }
+    else if(package == "tidytransit"){
+      gtfs_feeds <- purrr::map(feeds, tidytransit::read_gtfs)
+      }
     return(gtfs_feeds)
-  } else{
+    }
+  else{
     # ----- Error message if there are no zip files in the provided directory
     stop("There are no zip files in the feed directory provided. Check {feed_dir} variable")
-
   }
 
 }
