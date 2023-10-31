1bd4386fc453e717c5418ac3971bb29b9a6c801b
Hussein Mahfouz <45176416+Hussein-Mahfouz@users.noreply.github.com>
2023-10-31 17:41:18 +0000

    maps on number of destinations reachable + transfers

--- code/eda_travel_time.R
+++ code/eda_travel_time.R
@@ -10,12 +10,13 @@ source("R/study_area_geographies.R")
 
 library(tidyverse)
 library(sf)
+library(tmap)
 
 # -------------------- read in the outputs
 
 # --- decide on geographic resolution
 
-geography <- "MSOA"
+geography <- "LSOA"
 
 # read in geography
 study_area <- st_read("data/interim/study_area_boundary.geojson")
@@ -32,12 +33,20 @@ tt_matrix <- arrow::read_parquet(paste0("data/processed/travel_times/", geograph
 # Get median values per origin
 tt_matrix_o <- tt_matrix %>%
   group_by(from_id, combination, departure_time) %>%
-  summarise(across(contains("_time"), median, na.rm = TRUE))
+  summarise(across(c(contains("_time"), n_rides), median, na.rm = TRUE),
+            # number of destinations that can be reached
+            reachable_destinations = n()) %>%
+  ungroup() %>%
+  mutate(n_rides = round(n_rides))
 
 # Get median values per destination
 tt_matrix_d <- tt_matrix %>%
   group_by(to_id, combination, departure_time) %>%
-  summarise(across(contains("_time"), median, na.rm = TRUE))
+  summarise(across(c(contains("_time"), n_rides), median, na.rm = TRUE),
+            # number of origins that can reach the destination
+            reachable_origins = n()) %>%
+  ungroup() %>%
+  mutate(n_rides = round(n_rides))
 
 
 
@@ -53,22 +62,103 @@ tt_matrix_o %>%
   theme(legend.position = "top")
 
 
-# PLOT: BAR - average time associated with each leg of the trip (access, egress, ride, transfer, wait) - bar chart showing difference between combinations
-tt_matrix_o %>% group_by(combination) %>%
-  select(-c(departure_time, total_time)) %>%
-  filter(combination != "car") %>%
-  summarise(across(contains("_time"), median, na.rm = TRUE)) %>%
-  pivot_longer(cols = contains("time"), names_to = "leg", values_to = "time") %>%
-  ggplot(aes(x = combination, y = time, fill = leg)) +
-  geom_bar(position = "stack", stat = "identity") +
+tt_matrix_o %>%
+  filter(grepl("pt_", combination)) %>%
+  filter(wait_time > 0) %>%
+  ggplot(aes(x = wait_time, fill = combination)) +
+  geom_histogram(binwidth = 5, position = "identity", alpha = 0.7) +
+  facet_wrap(~combination, scales = "fixed") +
+  labs(title = "Wait time distribution at different times of day", subtitle = "Average per origin", x = "Time (min)", y = "No. of origins") +
   theme_minimal() +
-  labs(title = "What makes up the total travel time?")
+  theme(legend.position = "top")
 
 
 
 
-# bivariate plot with transfer time and no. of transfers  https://r-tmap.github.io/tmap/articles/tmap_sneak_peek.html#multivariate-scales
+
+
+
+# ----- MAP: bivariate plot with transfer time and no. of transfers  https://r-tmap.github.io/tmap/articles/tmap_sneak_peek.html#multivariate-scales
+
+
+
 
 # number of reachable destinations
    # expanded dataset - any row with total_time == NA means you can't reach
    # group_by origin, combination, departure_time -> count number of rows where total_time !- NA
+
+tm_shape(tt_matrix_o %>%
+           filter(combination != "car") %>%
+           left_join(study_area %>%
+                       select(OBJECTID) %>%
+                       mutate(OBJECTID = as.character(OBJECTID)),
+                     by = c("from_id" = "OBJECTID")) %>%
+           st_as_sf()) +
+  tm_fill(col = 'reachable_destinations',
+          title = "Destinations Reachable (120 minutes)",
+          legend.is.portrait = FALSE) +
+  tm_facets(by="combination",
+            nrow = 2,
+            free.coords=FALSE)  +  # so that the maps aren't different sizes
+  tm_layout(fontfamily = 'Georgia',
+            main.title = "Number of reachable destinations from each origin", # this works if you need it
+            main.title.size = 1.3,
+            main.title.color = "azure4",
+            main.title.position = "left",
+            legend.outside = TRUE,
+            legend.outside.position = "bottom",
+            frame = FALSE)
+
+# average number of transfers from origin (origin anchored)
+
+tm_shape(tt_matrix_o %>%
+           filter(combination != "car") %>%
+           left_join(study_area %>%
+                       select(OBJECTID) %>%
+                       mutate(OBJECTID = as.character(OBJECTID)),
+                     by = c("from_id" = "OBJECTID")) %>%
+           mutate(n_rides = as.character(n_rides)) %>%
+           st_as_sf()) +
+  tm_fill(col = 'n_rides',
+          breaks = c(0, 1, 2, 3),
+          title = "Number of transfers",
+          legend.is.portrait = FALSE) +
+  tm_facets(by="combination",
+            nrow = 2,
+            free.coords=FALSE)  +  # so that the maps aren't different sizes
+  tm_layout(fontfamily = 'Georgia',
+            main.title = "Average number of transfers from origin", # this works if you need it
+            main.title.size = 1.3,
+            main.title.color = "azure4",
+            main.title.position = "left",
+            legend.outside = TRUE,
+            legend.outside.position = "bottom",
+            frame = FALSE)
+
+# average number of transfers to destination (destination anchored)
+
+tm_shape(tt_matrix_d %>%
+           filter(combination != "car") %>%
+           left_join(study_area %>%
+                       select(OBJECTID) %>%
+                       mutate(OBJECTID = as.character(OBJECTID)),
+                     by = c("to_id" = "OBJECTID")) %>%
+           mutate(n_rides = as.character(n_rides)) %>%
+           st_as_sf()) +
+  tm_fill(col = 'n_rides',
+          title = "Number of transfers",
+          legend.is.portrait = FALSE) +
+  tm_facets(by="combination",
+            nrow = 2,
+            free.coords=FALSE)  +  # so that the maps aren't different sizes
+  tm_layout(fontfamily = 'Georgia',
+            main.title = "Average number of transfers to destination", # this works if you need it
+            main.title.size = 1.3,
+            main.title.color = "azure4",
+            main.title.position = "left",
+            legend.outside = TRUE,
+            legend.outside.position = "bottom",
+            frame = FALSE)
+
+
+
