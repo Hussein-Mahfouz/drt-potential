a23d5ecd1ee9b02b4da6729705f6ade6f41895a7
Hussein Mahfouz <45176416+Hussein-Mahfouz@users.noreply.github.com>
2024-01-12 16:51:16 +0000

    nest + map for merging all combinations simultaneously

--- R/simplify_graph.R
+++ R/simplify_graph.R
@@ -7,28 +7,28 @@
 simplify_graph = function(layer, radius){
   # --- 1. convert to sfnetwork
   layer_net <- layer %>%
-    sf::st_transform(3857) %>%  # I want metric crs for grouping nodes based on distance
-    sfnetworks::as_sfnetwork(., directed = FALSE)
+    st_transform(3857) %>%  # I want metric crs for grouping nodes based on distance
+    as_sfnetwork(., directed = FALSE)
 
   # --- 2. group nodes based on proximity
   # -- a. Retrieve the coordinates of the nodes.
   node_coords = layer_net %>%
-    tidygraphh::activate("nodes") %>%
-    sf::st_coordinates()
+    activate("nodes") %>%
+    st_coordinates()
   # -- b. Cluster the nodes with the DBSCAN spatial clustering algorithm.
   # We set eps = x such that:
   # Nodes within a distance of x meters from each other will be in the same cluster.
   # We set minPts = 1 such that:
   # A node is assigned a cluster even if it is the only member of that cluster.
-  clusters = dbscan::dbscan(node_coords, eps = radius, minPts = 1)$cluster
+  clusters = dbscan(node_coords, eps = radius, minPts = 1)$cluster
   # Add the cluster information to the nodes of the network.
   clustered =  layer_net %>%
-    tidygraph::activate("nodes") %>%
-    dplyr::mutate(cls = clusters)
+    activate("nodes") %>%
+    mutate(cls = clusters)
   # -- c. contract: merge nodes together based on clustering results
   layer_cont = tidygraph::convert(
     clustered,
-    sfnetworks::to_spatial_contracted,
+    to_spatial_contracted,
     cls,
     # simplify - FALSE so that we keep multiple edges.
     # All edges that have the same from and to node will then be grouped in the next step
@@ -37,7 +37,7 @@ simplify_graph = function(layer, radius){
 
   # replace NA values with 0 so that summation works!
   layer_cont <- layer_cont %>%
-    tidygraph::activate("edges") %>%
+    activate("edges") %>%
     mutate(commute_all = replace_na(commute_all, 0))
 
   # --- 3. merge multiple edges (edges that have the same end nodes)
@@ -49,18 +49,19 @@ simplify_graph = function(layer, radius){
   # Simplify the network with to_spatial_simple.
   layer_simp = tidygraph::convert(
     layer_cont,
-    sfnetworks::to_spatial_simple,
+    to_spatial_simple,
     # `Vehicles/Hour` will be summed, all other attributes we will take the first row in the group
     summarise_attributes = list(commute_all = "sum", "first"))
 
   # --- 4. Convert to sf in order to save
   layer_simp <- layer_simp %>%
-    tidygraph::activate("edges") %>%
+    activate("edges") %>%
     st_as_sf() %>% st_transform(4326) %>%
-    dplyr::select(commute_all)
+    select(commute_all)
 
   return(layer_simp)
 }
+
 
 
 
--- code/pt_or_drt.R
+++ code/pt_or_drt.R
@@ -2,12 +2,17 @@
 
 library(tidyverse)
 library(sf)
-library(dodgr)
+library(stplanr)
+library(tmap)
+# network analysis - merging nodes and edges
+library(sfnetworks)
+library(dbscan)
+library(tidygraph)
 
 
 source("R/study_area_geographies.R")
 source("R/filter_od_matrix.R")
-
+source("R/simplify_graph.R")
 
 # ----------- 1. Load in the data
 
@@ -25,12 +30,17 @@ study_area = study_area_geographies(study_area = study_area,
 study_area <- study_area %>%
   st_cast("MULTIPOLYGON")
 
-
-
-
 # ----- Travel time and demand matrix
 
 od_demand <- arrow::read_parquet(paste0("data/raw/travel_demand/od_census_2021/demand_study_area_", tolower(geography), ".parquet"))
+
+# ----- shortest path geometries
+
+od_shortest_paths <- readRDS(paste0("data/processed/travel_times/", geography, "/shortest_path_car.Rds"))
+
+# --- path to save plots
+plots_path <- paste0("data/processed/plots/eda/flows_no_direct/", geography, "/")
+
 
 # ---------- 2. Prep the loaded data
 
@@ -54,7 +64,7 @@ study_area_centroid <- study_area %>%
   # transform to lon lat
   st_transform(4326)
 
-# ---------- 2. METHOD A: Overline
+# ---------- 3. METHOD A: Overline
 
 # --- a. Filter OD pairs to those needing an improved service
 
@@ -62,57 +72,255 @@ study_area_centroid <- study_area %>%
 od_demand_no_direct = od_demand %>%
   filter(n_rides > 1)
 
-#test
-od_demand_2 <- od_demand %>% filter(combination == "pt_wkday_morning")
-# --- b. get shortest path geometry for each OD pair
-od_demand_no_direct_t <- od_demand_no_direct %>%
-  filter(combination == "pt_wkday_morning")
+# --- b. add shortest path geometry
+
+od_demand_no_direct <- od_demand_no_direct %>%
+  left_join(od_shortest_paths, by = c("from_id", "to_id")) %>%
+  st_as_sf()
+
+# --- placeholder: nest sf by combination in order to apply overline and merge per group
+
+# --- c. OPTION 1: use overline to get total flow on road network
+
+# nest sf data by combination and apply overline per nested df
+od_demand_filtered <- od_demand_no_direct %>%
+  group_by(combination) %>%
+  nest()
+
+# use map to apply overline function to all groups
+od_demand_overline <- od_demand_filtered %>%
+  # create a new df for each group
+  mutate(demand = map(data, ~ stplanr::overline(sl = .x,
+                                                attrib = "commute_all",
+                                                ncores = 3,
+                                                fun = sum))) %>%
+  select(-data) %>%
+  # turn back into one big sf
+  unnest(demand)
 
 
-# get origin and destination dfs for routing
-from = od_demand_2 %>%
-  select(Origin) %>%
-  inner_join(study_area_centroid, by = c("Origin" = "geoID")) %>%
-  st_as_sf() %>%
-  st_coordinates(.) %>%
-  as.matrix()
-
-to = od_demand_2 %>%
-  select(Destination) %>%
-  inner_join(study_area_centroid, by = c("Destination" = "geoID")) %>%
-  st_as_sf() %>%
-  st_coordinates(.) %>%
-  as.matrix()
-
-od_demand_2 %>%
-  select(Origin, Destination, commute_all) %>%
-  pivot_wider(names_from = Destination, values_from = commute_all) %>%
-  tibble::column_to_rownames(var = "Origin") %>%
-  mutate(across(everything(), .fns = ~replace_na(.,0))) -> x
-
-# --- extract street network for routing
-streetnet_sc = dodgr::dodgr_streetnet_sc(pts = st_coordinates(study_area_centroid$geometry), )
-streetnet_sf = dodgr::dodgr_streetnet(pts = st_coordinates(study_area_centroid$geometry))
 
 
-# --- weight the street network
-
-# # write the weighting profile locally so you can edit it
-# dodgr::write_dodgr_wt_profile(file = "data/external/dodgr/weighting_profile_template")
-#
-# weight_profile = jsonlite::read_json("data/external/dodgr/weighting_profile_template.json")
-
-graph <- weight_streetnet(streetnet_sc, wt_profile = "motorcar")
 
 
-# --- routing
-graph_flow <- dodgr_flows_aggregate(graph = graph,
-                                    from = from,
-                                    to = to,
-                                    flows = x,
-                                    contract = TRUE)
 
-# --- c. Use overline to get total demand on each road segment
+# --- d. OPTION 2: use overline + network simplification methods to remove parallel lines
+
+
+# use map to apply overline() + simplify_graph() function to all groups
+od_demand_overline_merge <- od_demand_filtered %>%
+  # create a new df for each group
+  # apply overline with simplify = FALSE. Keep split vertices so that you apply sfnetwork morphers
+  mutate(demand = map(data, ~ stplanr::overline(sl = .x,
+                                                attrib = "commute_all",
+                                                ncores = 3,
+                                                simplify = FALSE,
+                                                fun = sum))) %>%
+  mutate(demand_merged = map(demand, ~ simplify_graph(layer = .x,
+                                                      radius = 35))) %>%
+  select(-c(data, demand)) %>%
+  # turn back into one big sf
+  unnest(demand_merged)
+
+
+
+
+# compare the results
+
+
+
+od_compare <- od_demand_filtered %>%
+  select(commute_all) %>%
+  mutate(approach = "no_processing") %>%
+  bind_rows(od_demand_overline %>%
+              mutate(approach = "overline")) %>%
+  bind_rows(od_demand_overline_merge %>%
+              mutate(approach = "overline_merge"))
+
+
+ggplot(od_compare %>%
+         filter(approach != "no_processing", combination = "pt_wkday_morning"),
+       aes(x = commute_all, fill = approach)) +
+  geom_histogram(binwidth = 50) +
+  facet_wrap(~approach, nrow = 4)
+
+
+tmap_mode("view")
+tmap_mode("plot")
+
+
+# Compare overline to base
+tm_shape(study_area) +
+  tm_borders(col = "grey60",
+             alpha = 0.5) +
+tm_shape(study_area) +
+  tm_fill(col = "indianred2", #grey80
+          alpha = 0.5) +
+tm_shape(od_compare %>%
+           filter(approach %in% c("no_processing", "overline"))) +
+  tm_lines(col = "commute_all",
+           lwd = "commute_all",
+           scale = 15,
+           palette = "Greens",
+           style = "jenks", # pretty
+           legend.lwd.show = FALSE,
+           alpha = 1,
+           title.col = "Aggregated demand",
+           legend.col.is.portrait = FALSE) +
+  tm_facets(by = "approach",
+            free.coords = FALSE,
+            nrow = 1) +
+  tm_layout(fontfamily = 'Georgia',
+            main.title = "OD demand aggregated along shortest paths",
+            main.title.size = 1.3,
+            main.title.color = "azure4",
+            main.title.position = "left",
+            legend.outside = TRUE,
+            legend.outside.position = "bottom",
+            legend.stack = "horizontal",
+            frame = FALSE) -> map_aggregate_flows_overline_vs_base
+
+map_aggregate_flows_overline_vs_base
+
+tmap_save(tm =  map_aggregate_flows_overline_vs_base, filename = paste0(plots_path, "map_aggregate_flows_overline_vs_base.png"), width = 15, dpi = 1080)
+
+
+
+# compare overline to overline + merge
+tm_shape(study_area) +
+  tm_borders(col = "grey60",
+             alpha = 0.5) +
+tm_shape(study_area) +
+  tm_fill(col = "indianred2",
+          alpha = 0.5) +
+  tm_shape(od_compare %>%
+             filter(approach %in% c("overline", "overline_merge"))) +
+  tm_lines(col = "commute_all",
+           lwd = "commute_all",
+           scale = 10,
+           palette = "Greens",
+           style = "jenks", # pretty
+           legend.lwd.show = FALSE,
+           alpha = 1,
+           title.col = "Aggregated demand",
+           title.lwd = "",
+           legend.col.is.portrait = FALSE) +
+  tm_facets(by = "approach",
+            free.coords = FALSE,
+            nrow = 1) +
+  tm_layout(fontfamily = 'Georgia',
+            main.title = "OD demand aggregated along shortest paths",
+            main.title.size = 1.3,
+            main.title.color = "azure4",
+            main.title.position = "left",
+            legend.outside = TRUE,
+            legend.outside.position = "bottom",
+            legend.stack = "horizontal",
+            frame = FALSE) -> map_aggregate_flows_overline_vs_merge
+
+map_aggregate_flows_overline_vs_merge
+
+tmap_save(tm =  map_aggregate_flows_overline_vs_merge, filename = paste0(plots_path, "map_aggregate_flows_overline_vs_merge.png"), width = 15, dpi = 1080)
+
+
+
+tm_shape(study_area) +
+  tm_borders(col = "grey60",
+             alpha = 0.5) +
+tm_shape(study_area) +
+  tm_fill(col = "indianred2",
+          alpha = 0.5) +
+tm_shape(od_demand_overline2_merge %>%
+             filter(commute_all > 1000)) +
+  tm_lines(col = "commute_all",
+           lwd = "commute_all",
+           scale = 10,
+           palette = "Greens",
+           style = "jenks",
+           legend.lwd.show = FALSE,
+           alpha = 1,
+           title.col = "Aggregated demand",
+           legend.col.is.portrait = FALSE) +
+  tm_layout(fontfamily = 'Georgia',
+            main.title = "OD demand aggregated along shortest paths",
+            main.title.size = 1.3,
+            main.title.color = "azure4",
+            main.title.position = "left",
+            legend.outside = TRUE,
+            legend.outside.position = "bottom",
+            legend.stack = "horizontal",
+            frame = FALSE) -> map_aggregate_flows_merge
+
+map_aggregate_flows_merge
+# save interactive map
+htmlwidgets::saveWidget(map_aggregate_flows_merge, file = paste0(plots_path, "map_aggregate_flows_merge.html"))
+
+
+
+
+
+# compare
+
+od_compare2 <- od_demand_fi %>%
+  select(commute_all) %>%
+  mutate(approach = "no_processing") %>%
+  bind_rows(od_demand_merge %>%
+              mutate(approach = "sfnet_merge"))
+
+
+
+
+tm_shape(study_area) +
+  tm_borders(col = "grey60",
+             alpha = 0.5) +
+  tm_shape(study_area) +
+  tm_fill(col = "grey80",
+          alpha = 0.5) +
+  tm_shape(od_compare2) +
+  tm_lines(col = "commute_all",
+           lwd = "commute_all",
+           scale = 10,
+           #palette = "Blues",
+           style = "quantile",
+           legend.lwd.show = FALSE,
+           alpha = 1,
+           title.col = "Aggregated demand",
+           legend.col.is.portrait = FALSE) +
+  tm_facets(by = "approach",
+            free.coords = FALSE,
+            nrow = 1) +
+  tm_layout(fontfamily = 'Georgia',
+            main.title = "OD demand aggregated along shortest paths",
+            main.title.size = 1.3,
+            main.title.color = "azure4",
+            main.title.position = "left",
+            legend.outside = TRUE,
+            legend.outside.position = "bottom",
+            legend.stack = "horizontal",
+            frame = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # not just indirect, but low frequency also
 od_demand_no_direct_2 <- od_demand %>%
