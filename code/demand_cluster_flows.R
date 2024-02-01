6215da5a771dd789bcee12ba141edc6fbbf3bca1
Hussein Mahfouz <45176416+Hussein-Mahfouz@users.noreply.github.com>
2024-02-01 18:25:53 +0000

    flow dissimilarity

--- code/demand_cluster_flows.R
+++ code/demand_cluster_flows.R
@@ -1,20 +1,178 @@
 library(tidyverse)
 library(sf)
-
-# Get od demand data (should be by mode)
-
-
+library(lwgeom)
+library(spatstat)
 
 
-# ------------ "Spatial Cluster Detection in Spatial Flow Data" ---------- #
+source("R/study_area_geographies.R")
+source("R/filter_od_matrix.R")
 
+
+########## ----------------------- Read in the data ----------------------- ##########
+
+# ----------- 1. Study area
+
+# --- administrative boundaries
+study_area <- st_read("data/interim/study_area_boundary.geojson")
+
+# convert to desired resolution
+geography = "MSOA"
+study_area = study_area_geographies(study_area = study_area,
+                                    geography = geography)
+
+study_area <- study_area %>%
+  st_cast("MULTIPOLYGON")
+
+# ----------- 2.  Census OD data
+
+# Demand (census) + supply (travel time) data
+
+od_demand <- arrow::read_parquet(paste0("data/raw/travel_demand/od_census_2021/demand_study_area_", tolower(geography), ".parquet"))
+
+# filter to specific combination
+# TODO: get seperate flows for car and pt, and keep two combinations
+od_demand <- od_demand %>%
+  filter(combination == "pt_wkday_morning")
+
+# rename columns as most functions are applied on generic column names
+from_id_col = paste0(geography, "21CD_home")
+to_id_col = paste0(geography, "21CD_work")
+
+od_demand = od_demand %>%
+  rename("Origin" = all_of(from_id_col),
+         "Destination" = all_of(to_id_col))
+########## ----------------------- Convert df to sf desire lines ----------------------- ##########
+
+
+# --- create desire lines and remove od pairs with very short distance
+
+# TODO: edit this to avoid clusters of very short flows
+  # "Density-based clustering for bivariate-flow data" (section 5.2): preprocessing step to avoid
+  # clusters of very short flows. this involves splitting the data into 3 chunks
+  # based on length (
+od_demand_filtered = filter_matrix_by_distance(zones = study_area,
+                                               od_matrix = od_demand,
+                                               dist_threshold = 500)
+
+
+
+########## ------------ "Spatial Cluster Detection in Spatial Flow Data" ---------- ##########
 
 # ----- STEP 1: Get distance between flows
 
-# Option 1: Flow distance
+# metric crs
+od_demand_filtered <- od_demand_filtered %>%
+  st_transform(3857)
 
-# check: st_distance(by_element = FALSE)
+# Add columns for coordinates of startpoint (X, Y) and endpoint (U, V)
 
+x <- od_demand_filtered %>%
+  mutate(x = as_tibble(st_coordinates(st_startpoint(od_demand_filtered)))$X,
+         y = as_tibble(st_coordinates(st_startpoint(od_demand_filtered)))$Y,
+         u = as_tibble(st_coordinates(st_endpoint(od_demand_filtered)))$X,
+         v = as_tibble(st_coordinates(st_endpoint(od_demand_filtered)))$Y) %>%
+  st_drop_geometry()
+
+# create flow ID column
+x <- x %>%
+  mutate(flow_ID = paste(Origin, Destination, sep = "-"))
+
+
+# filter to specific origin
+x_origin_i <- x %>% filter(Origin == study_area$MSOA21CD[1])
+
+# create grid
+x_origin_i_grid <- expand_grid(flow_ID_a = x_origin_i$flow_ID,
+                               flow_ID_b = x_origin_i$flow_ID)
+
+# add the coordinate data
+x_origin_i_grid2 <- x_origin_i_grid %>%
+  # --- add flow_a coordinates
+  inner_join(x %>%
+              select(flow_ID, x, y, u, v, distance_m),
+            by = c("flow_ID_a" = "flow_ID")) %>%
+  # rename coordinate columns by adding a suffix
+  rename_with(.fn = ~ paste0(.x, "_i"), .cols = c("x", "y", "u", "v", "distance_m")) %>%
+  # --- add flow_b coordinates
+  inner_join(x %>%
+               select(flow_ID, x, y, u, v, distance_m),
+             by = c("flow_ID_b" = "flow_ID")) %>%
+  # rename coordinate columns by adding a suffix
+  rename_with(.fn = ~ paste0(.x, "_j"), .cols = c("x", "y", "u", "v", "distance_m"))
+
+# ----- get "Flow Distance" and "Flow Dissimilarity"
+
+# Flow Distance
+
+x_origin_i_grid2 <- x_origin_i_grid2 %>%
+  mutate(fd = sqrt(0.5 * ((x_i - x_j)^2 + (y_i - y_j)^2) +
+              0.5 * ((u_i - u_j)^2 + (v_i - v_j)^2)),
+         fds = sqrt((0.5 * ((x_i - x_j)^2 + (y_i - y_j)^2) +
+                      0.5 * ((u_i - u_j)^2 + (v_i - v_j)^2)) / (distance_m_i * distance_m_j)))
+
+
+
+flow_distance = function(study_area, flows){
+  # empty list to store results for each origin
+  results <- vector(mode = "list", length = nrow(study_area))
+
+  for (i in 1:nrow(study_area)){
+
+    #status updates
+    print(paste0("Getting distances for Origin: ", i, " ....."))
+
+    # filter to specific origin
+    flows_origin_i <- flows %>% filter(Origin == study_area$MSOA21CD[1])
+
+    # create grid
+    flows_origin_i_grid <- expand_grid(flow_ID_a = flows_origin_i$flow_ID,
+                                   flow_ID_b = flows_origin_i$flow_ID)
+
+    # add the coordinate data
+    flows_origin_i_grid <- flows_origin_i_grid %>%
+      # --- add flow_a coordinates
+      inner_join(flows %>%
+                   select(flow_ID, x, y, u, v, distance_m),
+                 by = c("flow_ID_a" = "flow_ID")) %>%
+      # rename coordinate columns by adding a suffix
+      rename_with(.fn = ~ paste0(.x, "_i"), .cols = c("x", "y", "u", "v", "distance_m")) %>%
+      # --- add flow_b coordinates
+      inner_join(flows %>%
+                   select(flow_ID, x, y, u, v, distance_m),
+                 by = c("flow_ID_b" = "flow_ID")) %>%
+      # rename coordinate columns by adding a suffix
+      rename_with(.fn = ~ paste0(.x, "_j"), .cols = c("x", "y", "u", "v", "distance_m"))
+
+    # ----- get "Flow Distance" and "Flow Dissimilarity"
+
+    # Flow Distance
+    flows_origin_i_grid <- flows_origin_i_grid %>%
+      mutate(fd = sqrt(0.5 * ((x_i - x_j)^2 + (y_i - y_j)^2) +
+                         0.5 * ((u_i - u_j)^2 + (v_i - v_j)^2)),
+             fds = sqrt((0.5 * ((x_i - x_j)^2 + (y_i - y_j)^2) +
+                           0.5 * ((u_i - u_j)^2 + (v_i - v_j)^2)) / (distance_m_i * distance_m_j)))
+
+    # add results to list
+    results[[i]] <- flows_origin_i_grid
+
+  }
+  return(results)
+}
+
+
+test <- flow_distance(study_area = study_area,
+                      flows = x)
+
+test2 <- bind_rows(test)
+
+
+
+hi <- spatstat.geom::psp(x0 = x$x, y0 = x$y, x1 = x$u, y1 = x$v,
+                         window <- spatstat.geom::as.owin(study_area %>%
+                                                            st_transform(3857)))
+
+
+hi2 <- hi %>% Lest(.)
 
 # Option 2: Flow Dissimilarity
 
