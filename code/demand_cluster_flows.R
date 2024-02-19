ed4d91f0274604db2500c9742eaffcc7936b93b7
Hussein Mahfouz <45176416+Hussein-Mahfouz@users.noreply.github.com>
2024-02-19 18:32:48 +0000

    parameter sensitivity for dbscan

--- code/demand_cluster_flows.R
+++ code/demand_cluster_flows.R
@@ -8,6 +8,7 @@ library(tmap)
 
 source("R/study_area_geographies.R")
 source("R/filter_od_matrix.R")
+source("R/dbscan_sensitivity.R")
 source("code/demand_cluster_flows_prep.R")
 
 
@@ -30,7 +31,9 @@ od_demand_jittered <- od_demand_jittered %>%
   # add distance column
   mutate(distance_m =  units::drop_units(sf::st_length(.)))
 
-
+# # split by distance
+# od_demand_jittered <- od_demand_jittered %>%
+#   filter(distance_m < 0.25*max(distance_m))
 
 
 # - Add columns for coordinates of startpoint (X, Y) and endpoint (U, V)
@@ -158,7 +161,32 @@ dist_mat <- distances %>%
 
 # ------------------------- CLUSTERING USING DBSCAN ------------------------- #
 
-#  ---------- STEP 1: Decide on Epsilon (ε) and minPTS
+
+
+
+### ---------- STEP 1: Create Weights for dbscan ---------- ###
+
+# We have an OD matrix where each flow vector represents n commuters. We need to weight each vector
+# by the number of commuters so that minPTS works correctly and doesn't treat each vector as 1 item.
+
+# The weights vector needs to match the rows of the distance matrix
+
+# --- get weights
+w <- dist_mat %>%
+  rownames_to_column(var = "flow_ID") %>%
+  select(flow_ID) %>%
+  # add commuting data to
+  inner_join(od_demand_jittered %>%
+               select(flow_ID, commute_all),
+             by = "flow_ID")
+
+# weight vector
+w_vec <- as.vector(w$commute_all)
+
+
+
+###  ---------- STEP 2: Decide on Epsilon (ε) and minPTS ---------- ###
+
 
 # epsilon (ε) is a parameter used to define the maximum distance threshold
 # for points to be considered as neighbors. minPTS is the minimum number of points
@@ -167,12 +195,12 @@ dist_mat <- distances %>%
 # ----- K-nearest neighbor knee plot
 # kNNdistplot(dist_mat, k = 100)
 
-# ----- Explore distance distribution of matrix
+# ----- Explore distance distribution of matrix (for epsilon)
 
 # distance distribution of the flows
 hist(distances$fds, breaks = 100)
 
-# ----- Monte Carlo simulation to get mean value of distances
+# ----- Monte Carlo simulation to get mean value of distances (for epsilon)
 
 # if we consider all flows, the distance is skewed by flows that START at the same O
 # or end at the same D, and this will give us clusters of flows that either start or end at the same point
@@ -193,27 +221,45 @@ results <- replicate(repetitions, mean_distance())
 hist(results)
 
 
-# ---------- STEP 2: Create Weights
+# ----- Sensitivity analysis(for different epsilon and minpts combinaitons)
 
-# We have an OD matrix where each flow vector represents n commuters. We need to weight each vector
-# by the number of commuters so that minPTS works correctly and doesn't treat each vector as 1 item.
-
-# The weights vector needs to match the rows of the distance matrix
-
-# --- get weights
-w <- dist_mat %>%
-  rownames_to_column(var = "flow_ID") %>%
-  select(flow_ID) %>%
-  # add commuting data to
-  inner_join(od_demand_jittered %>%
-               select(flow_ID, commute_all),
-             by = "flow_ID")
-
-# weight vector
-w_vec <- as.vector(w$commute_all)
+# function to get clustering results for many combinations
+dbscan_sensitivity_res <- dbscan_sensitivity(distance_matrix = dist_mat,
+                                             options_epsilon <- c(0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 5, 6, 7, 7.5, 8, 9),
+                                             options_minpts <- c(50, 100, 150, 175, 200, 250, 300, 400, 500, 700, 1000, 1500, 3000, 5000, 10000),
+                                             weights = w_vec,
+                                             flows = st_drop_geometry(od_demand_jittered)
+                                             )
 
 
-# ---------- STEP 3: Cluster
+arrow::write_parquet(dbscan_sensitivity_res, paste0("data/interim/travel_demand/", geography, "/od_demand_clustering_sensitivity.parquet"))
+# dbscan_sensitivity_res <- arrow::read_parquet(paste0("data/interim/travel_demand/", geography, "/od_demand_clustering_sensitivity.parquet"))
+
+dbscan_sensitivity_res %>%
+  filter(cluster != 0) %>%
+  ggplot(aes(x = cluster, y = size, fill = commuters_sum)) +
+  geom_col() +
+  facet_wrap(~id, scales = "fixed") +
+  labs(title = "Sensitivity analysis for clustering", subtitle = "Varying {eps} and {minPts}", x = "Cluster no.", y = "No. of commuters") +
+  theme_minimal()
+
+dbscan_sensitivity_res %>%
+  filter(cluster != 0) %>%
+  group_by(id) %>%
+  #mutate(clusters = n()) %>%
+  mutate(clusters = sum(size > 5)) %>%
+  ungroup() %>%
+  filter(clusters > 5) %>%
+  ggplot(aes(x = cluster, y = size, fill = commuters_sum)) +
+  geom_col() +
+  facet_wrap(~id, scales = "fixed") +
+  labs(title = "Sensitivity analysis for clustering", subtitle = "Varying {eps} and {minPts}", x = "Cluster no.", y = "No. of commuters") +
+  theme_minimal()
+
+
+
+###  ---------- STEP 3: Cluster ---------- ###
+
 
 # cluster option 1: border points assigned to cluster
 cluster_dbscan = dbscan::dbscan(dist_mat,
@@ -221,6 +267,12 @@ cluster_dbscan = dbscan::dbscan(dist_mat,
                                 eps = 8, # 9.5
                                 #borderPoints = FALSE,
                                 weights = w_vec)
+# # for splitted distance
+# cluster_dbscan = dbscan::dbscan(dist_mat,
+#                                 minPts = 70, # 125
+#                                 eps = 6.5, # 9.5
+#                                 #borderPoints = FALSE,
+#                                 weights = w_vec)
 
 # # cluster option 2: border points not assigned to cluster
 # cluster_dbscan = dbscan::dbscan(dist_mat,
@@ -432,3 +484,5 @@ big_clusters %>%
 
 plot(st_geometry(study_area %>% st_transform(3857)))
 plot(st_geometry(x), add = TRUE, col = "red")
+
+
